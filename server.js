fcc91780087a6fde8baf4a21e3b53c47341ae811
Anosh Malik <anoshmalik.am@gmail.com>
2023-01-08 21:21:07 +0000

    First commit- finished lesson's video

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 9090;
+
+
+const foodArray = [
+
+    {
+        "name": "biryani",
+        "flavour" : "spicy",
+    },
+    {
+        "name": "nihari",
+        "flavour" : "spicy"
+    },
+    {
+        "name": "mithai",
+        "flavour" : "sweet"
+    }
+];
 
 
 app.get("/", (request, response) => {
-    response.send();
- });
+    console.log("You've reached my API");
+    response.send("Here's your response!!!");
+    console.log(request.query, "<---Request.query");
+});
+
+app.get("/trainees", (request, response) => { 
+    console.log(request.query, "<---Request.query");
+
+    response.send(`Response : FROM TRAINEES ENDPOINT \n\  ${foodArray}`);
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on ${port}`);
+    console.log(`Example app listening now..`);
 });
 
 
-// console.log(app);
