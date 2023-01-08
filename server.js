18255747c4b87a0ecc6865bd70a4d2f3d735bf37
levilu800b <levilu800b@gmail.com>
2023-01-08 19:41:41 +0000

    Added starter

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-// Starter
+// create a backend server 
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
