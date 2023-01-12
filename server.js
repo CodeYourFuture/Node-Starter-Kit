c1b55f88ffbb7824933b78813d2e072a632af437
Anosh Malik <anoshmalik.am@gmail.com>
2023-01-12 13:30:48 +0000

    Started Week 2 Prep

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
-const express = require("express");
-const app = express();
-const port = 9090;
+// PREVIOUS CODE FOR WEEK 1
+// const express = require("express");
+// const app = express();
+// const port = 9090;
 
+// const foodArray = [
 
-const foodArray = [
+//     {
+//         "name": "biryani",
+//         "flavour" : "spicy",
+//     },
+//     {
+//         "name": "nihari",
+//         "flavour" : "spicy"
+//     },
+//     {
+//         "name": "mithai",
+//         "flavour" : "sweet"
+//     }
+// ];
 
-    {
-        "name": "biryani",
-        "flavour" : "spicy",
-    },
-    {
-        "name": "nihari",
-        "flavour" : "spicy"
-    },
-    {
-        "name": "mithai",
-        "flavour" : "sweet"
-    }
-];
+// app.get("/", (request, response) => {
+//     console.log("You've reached my API");
+//     response.send("Here's your response!!!");
+//     console.log(request.query, "<---Request.query");
+// });
 
+// app.get("/trainees", (request, response) => {
+//     console.log(request.query, "<---Request.query");
 
-app.get("/", (request, response) => {
-    console.log("You've reached my API");
-    response.send("Here's your response!!!");
-    console.log(request.query, "<---Request.query");
-});
+//     response.send(`Response : FROM TRAINEES ENDPOINT \n\  ${foodArray}`);
+// })
 
-app.get("/trainees", (request, response) => { 
-    console.log(request.query, "<---Request.query");
+// app.listen(port, () => {
+//     console.log(`Example app listening now..`);
+// });
 
-    response.send(`Response : FROM TRAINEES ENDPOINT \n\  ${foodArray}`);
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening now..`);
-});
-
-
+// PREVIOUS CODE ABOVE FOR WEEK 1
