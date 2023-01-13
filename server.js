d796428fa3e9f1ab81d62ce583ec2a5a7e0c6443
Marryggk <99277162+Meronggk@users.noreply.github.com>
2023-01-13 22:00:39 +0000

    added some data

---
+++ server.js
@@ -0,0 +1,19 @@
+
+
+const express = require("express");
+const app = express();
+
+const images = [
+  [
+    {
+        id: 1,
+        title: 'Pirates of the caribean',
+
+    },
+    {
+        id: 2,
+        title: 'Pirates of the caribean2'
+    }
+  ]
+]
+
