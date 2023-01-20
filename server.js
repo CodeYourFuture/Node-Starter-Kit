8839ac0c25f9799d988808081b9dcff3245620e7
khmdagal <khmdagal@gmail.com>
2023-01-20 18:44:25 +0000

    Prep work for Node W3

--- server.js
+++ server.js
@@ -1,22 +1,56 @@
-
 const express = require("express");
 const app = express();
 const port = 9090;
+const fs = require("fs").promises;
 
-// creating server
-const trainees = [
-  { name: "Ali", age: 20, Region: "NW", week: "JS2W2" },
-  { name: "Adam", age: 20, Region: "London", week: "ReactW1" },
-];
+app.use(express.json());
 
+//----bringing data-------
 
+// ---------   Createing routes  ------------
 
-app.get("/trainees", (req, res) => {
-  const region = req.query.Region
-  const filteredByRegion = trainees.filter(el => el.Region === region)
-  res.send({filteredByRegion});
+// reading All albums
+app.get("/albums", (req, res) => {
+  fs.readFile("./albums.json", "utf-8").then((movie) => {
+    movie = JSON.parse(movie);
+    res.status(200).send(movie);
+  });
+});
+
+// reading albums by id
+app.get(`/albums/:id`, (req, res) => {
+  const id = +req.params.id;
+  const index = fs.readFile("./albums.json", "utf-8").then((movie) => {
+    const searchedAlbum = JSON.parse(movie).findIndex(
+      (album) => +album.id === +req.params.id
+    );
+    res.status(200).send(searchedAlbum);
+  });
+  console.log(index);
+  //const theAlbum = albums.filter((album) => album.id === id);
+});
+
+// PUT rount
+
+/* Todo
+use put method with fs modules
+*/
+app.put(`/albums/:id`, (req, res) => {
+  const requestBody = req.body;
+
+  const albumIndex = albums.findIndex((album) => +album.id === +req.params.id);
+  const theOldAlbum = albums[albumIndex];
+
+  const newAlbum = {
+    album: requestBody.album,
+    artist: requestBody.artist,
+    id: theOldAlbum.id, // here I want don't want to change the old id
+  };
+
+  albums.splice(albumIndex, 1, newAlbum);
+  res.status(200).send(theOldAlbum); // returning the replaced album
 });
 
 app.listen(port, () => {
-  console.log(`CYF server is secceffully launched ${port}`);
+  console.log(`Server is runing ${port}`);
 });
