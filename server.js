21b67fdb3ce5db14448f4739cfd90ea256542035
Zobeir <rigi.zobir@yahoo.com>
2023-03-23 22:36:13 +0000

    ive sent a message to client

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+app.get("/", function (req, res) {
+  console.log(res);
+  console.log(req);
 
+  res.send("Yay Node!");
+});
 app.listen(3000, () =>
   console.log("Server is listening on port 3000. Ready to accept requests!")
 );
