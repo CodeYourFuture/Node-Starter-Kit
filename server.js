cfdb89dde3076e9e5fb88f0a5e8cc479ef9d2e8b
farzaneh-haghani <farzaneh.haghani@gmail.com>
2023-03-24 22:46:33 +0000

    week1 done

--- server.js
+++ server.js
@@ -2,8 +2,28 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!, Yay Node!");
+  const searchQuery = req.query.search;
+  res.send(`Hello World!, Yay Node! You searched for ${searchQuery}`);
   // console.log(req);
+});
+
+app.get("/node", (req, res) => {
+  res.send("We are in Node endpoint!");
+});
+
+app.get("/codeyourfuture", (req, res) => {
+  res.send("We are in codeyourfuture endpoint!")
+});
+
+app.get("/chocolate", (req, res) => {
+  const amount = req.query.amount;
+  res.send(`You asked for ${amount} chocolate!`);
+});
+
+app.get("/multiply", (req, res) => {
+  const value1 = req.query.value1;
+  const value2 = req.query.value2;
+  res.send(`The multiply is ${value1 * value2}`);
 });
 
 app.listen(3000, () => {
