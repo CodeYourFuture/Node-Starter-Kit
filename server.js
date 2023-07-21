fdc7b3be178a28a878d61c15e39e7277e8cc452e
Afsha10 <unityofbeing1@gmail.com>
2023-07-21 23:52:55 +0100

    Week 2 Lessons 3.1: Get All Albums & 3.3: Install  Nodemon
    Completed Ex 3.1 : Get All Albums
    a)  In server.js Add the endpoint for GET /albums.
    b) Test the endpoint with Postman. GET /albums should return a JSON reply with the array we specified.
    c) Add another item to the array and test that the GET /albums returns three items.
    
    Completed Ex 3.3: Install Nodemon, a handy npm package
    
    npm install --save-dev nodemon
    
    Made sure the package is added to your package.json, added this line to the script:
    
    "scripts": {
      "start": "node server.js",
      "dev": "nodemon server.js"
    },

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, function () {
-    console.log("Server is listening on port 3000. Ready to accept requests!");
-});
+const albumsData = [
+  {
+    albumId: "10",
+    artistName: "Beyoncé",
+    collectionName: "Lemonade",
+    artworkUrl100:
+      "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
+    releaseDate: "2016-04-25T07:00:00Z",
+    primaryGenreName: "Pop",
+    url: "https://www.youtube.com/embed/PeonBmeFR8o?rel=0&amp;controls=0&amp;showinfo=0",
+  },
+  {
+    albumId: "11",
+    artistName: "Beyoncé",
+    collectionName: "Dangerously In Love",
+    artworkUrl100:
+      "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
+    releaseDate: "2003-06-24T07:00:00Z",
+    primaryGenreName: "Pop",
+    url: "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0",
+  },
+  {
+    albumId: "12",
+    artistName: "The Beatles",
+    collectionName: "With the Beatles",
+    artworkUrl100:
+      "https://en.wikipedia.org/wiki/With_the_Beatles#/media/File:Withthebeatlescover.jpg",
+    releaseDate: "1963-11-22T07:00:00Z",
+    primaryGenreName: "Rock",
+    url: "https://www.youtube.com/watch?v=UVKU6SevefY&list=PLycVTiaj8OI-XRoKhxMmBhnsefIc77M2_",
+  },
+];
 
 app.get("/", function(req, res) {
     console.log(req);
-    res.send("Welcome to Afsha's bubble!")
+    res.send("Welcome to Alisha's bubble!")
 });
 
 app.get("/node", function(req, res) {
@@ -15,7 +44,7 @@ app.get("/node", function(req, res) {
 })
 
 app.get("/codeyourfuture", function (req, res) {
-  res.send("Imagine that all the programmers learning only JavaScript...");
+    res.send("Imagine that all the programmers learning only JavaScript...");
 });
 
 app.get("/chocolate", function (req, res) {
@@ -30,3 +59,11 @@ app.get("/multiply", function (req, res) {
     let result = value1 * value2;
     res.send(`We need ${result} boxes of chocolates for our class pupils`)
 })
+
+app.get("/albums", function (req, res) {
+  res.send({ albumsData });
+});
+
+app.listen(35981, function () {
+    console.log("Server is listening on port 35981. Ready to accept requests!");
+});
